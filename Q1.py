ec998d6803459426f61e29d411ab3f14be4d5e04
gokuno1 <aniketbidgar2926@gmail.com>
2022-11-20 21:11:27 +0530

    models

--- Q1.py
+++ Q1.py
@@ -6,7 +6,13 @@ import pytest
 iris = load_iris()
 X, y = iris.data, iris.target
 
-X_train1, X_test1, y_train1, y_test1 = train_test_split(X, y, test_size=0.2, random_state=42)
-X_train2, X_test2, y_train2, y_test2 = train_test_split(X, y, test_size=0.2)
+X_train1, X_test1, y_train1, y_test1 = train_test_split(X, y, random_state=42)
 
-assert len(X_train1) == len(X_train2)
+X_train2, X_test2, y_train2, y_test2 = train_test_split(X, y, random_state=0)
+
+
+def validate(train1, train2):
+    assert train1.all() == train2.all()
+
+
+validate(X_train1,X_train2)
